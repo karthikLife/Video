f09b14e8fddcc3c379522ee477d3ffe2f2895f38
Benkarsam <benkarsam@gmail.com>
2017-01-25 14:38:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/enocksam/video/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.enocksam.video;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.MyButton);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        VideoViewActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
